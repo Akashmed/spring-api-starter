d584549ba0c5b56e54b554520e0f3480cfd78249
Akashmed <akashmed3@gmail.com>
2026-01-30 01:12:51 +0600

    implement CheckoutController, process checkout

---
+++ src/main/java/com/codewithmosh/store/dtos/CheckoutRequest.java
@@ -0,0 +1,13 @@
+package com.codewithmosh.store.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class CheckoutRequest {
+    @NotNull(message = "cart ID is required")
+    private UUID cartId;
+}
